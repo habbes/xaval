651e5fc60e905dedb20f3581a9a3be9e23109d48
cassilde muhoza <cassildemuhoza@MacBook-Pro-de-cassilde.local>
2018-06-10 22:08:59 +0300

    Add edge detection sample

--- src/ui/editor/editor.ts
+++ src/ui/editor/editor.ts
@@ -1,5 +1,5 @@
 import { Editor } from '@/types';
-import INITIAL_CODE from '@/samples/camera';
+import INITIAL_CODE from '@/samples/edge-detection';
 import { EditorProvider } from './types';
 import { createAceEditor } from './ace';
 

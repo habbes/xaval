79398a516f20fda813a0d005ff558e7119549271
Clément Habinshuti <hello@habbes.xyz>
2018-06-12 22:19:29 +0300

    Tweak initial code

--- src/samples/empty.ts
+++ src/samples/empty.ts
@@ -1,5 +1,6 @@
 export default
 `// write your code here
 // check out the code samples if you need help getting started
-// To learn more about OpenCV.js, check out the tutorials at: https://docs.opencv.org/3.4.1/d5/d10/tutorial_js_root.html
+// To learn more about OpenCV.js, check out the tutorials at:
+// https://docs.opencv.org/3.4.1/d5/d10/tutorial_js_root.html
 `;

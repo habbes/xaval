8115ebe87f3e0e3181a2d64cd7f26126728062a8
Clément Habinshuti <hello@habbes.xyz>
2018-06-12 22:02:46 +0300

    Add link to OpenCV tutorial in initial code

--- src/samples/empty.ts
+++ src/samples/empty.ts
@@ -1,4 +1,5 @@
 export default
 `// write your code here
 // check out the code samples if you need help getting started
+// To learn more about OpenCV.js, check out the tutorials at: https://docs.opencv.org/3.4.1/d5/d10/tutorial_js_root.html
 `;

ce3ab7a38c7e69aadb35d57443fcab3e9b9a01c8
Clément Habinshuti <hello@habbes.xyz>
2018-06-24 16:07:10 +0300

    Add video sample code

--- src/samples/video.ts
+++ src/samples/video.ts
@@ -1,8 +1,18 @@
 export default
 `const { files, imageViewer } = xaval.io;
 
+// import video file in the file library
+
+// read imported video
 const video = files.readVideo('file1');
+
+// get video stream and attach it to the image viewer
 const stream = video.getStream({ fps: 30 });
 stream.pipe(imageViewer);
+
+// enable the following line if you want the video to loop
+// video.looping = true
+
+// play the video
 video.play();
 `;

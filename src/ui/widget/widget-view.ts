e8230e707f8b15fe5d8e356669aab7f557cd3e8b
Clément Habinshuti <hello@habbes.xyz>
2018-06-28 13:17:57 +0300

    Update widget view controls when wiget model params are updated

--- src/ui/widget/widget-view.ts
+++ src/ui/widget/widget-view.ts
@@ -7,15 +7,53 @@ import { createCheckbox, createSlider, createSelect, createText } from './contro
 export class WidgetView {
     readonly el: HTMLElement;
     readonly model: WidgetModel;
+    readonly controls: { [name: string]: WidgetControlContainerPair } = {};
 
     constructor (model: WidgetModel) {
-        this.el = createHtml(model);
         this.model = model;
+        this.el = this.createHtml();
+        this.model.onParamUpdated(this.updateParam.bind(this));
     }
 
     setId (id: string) {
         this.el.id = id;
         this.el.querySelector('.widget-id').textContent = `#${id}`;
+    }
+
+    createHtml () {
+        const model = this.model;
+        const tpl = 
+        `<div class="widget-header">
+            <span class="widget-title">${model.opts.name}</span>
+            <span class="widget-id"></span>
+        </div>
+        <div class="widget-content">
+            <div class="params-container">
+            </div>
+        </div>`;
+        let node: WidgetHtmlElement = <WidgetHtmlElement>document.createElement('div');
+        node.className = 'widget';
+        node.innerHTML = tpl;
+        // TODO: is it necessary to create a node.widget reference?
+        node.widget = model;
+        const params = model.opts.params;
+        const paramContainer = node.querySelector('.params-container');
+        for (let paramName in params) {
+            const control = createParamControl(paramName, model);
+            paramContainer.appendChild(control.container);
+            this.controls[paramName] = control;
+        }
+        const header = <HTMLDivElement>node.querySelector('.widget-header');
+        header.draggable = true;
+        header.ondragstart = (ev) => {
+            ev.dataTransfer.setData('id', node.id);
+            ev.dataTransfer.dropEffect = 'move';
+        };
+        return node;
+    }
+
+    updateParam (name: string, value: any) {
+        this.controls[name].control.update(name, this.model);
     }
 }
 
@@ -23,37 +61,7 @@ interface WidgetHtmlElement extends HTMLDivElement {
     widget: WidgetModel;
 }
 
-function createHtml (model: WidgetModel) {
-    const tpl = 
-    `<div class="widget-header">
-        <span class="widget-title">${model.opts.name}</span>
-        <span class="widget-id"></span>
-    </div>
-    <div class="widget-content">
-        <div class="params-container">
-        </div>
-    </div>`;
-    let node: WidgetHtmlElement = <WidgetHtmlElement>document.createElement('div');
-    node.className = 'widget';
-    node.innerHTML = tpl;
-    // TODO: is it necessary to create a node.widget reference?
-    node.widget = model;
-    const params = model.opts.params;
-    const paramContainer = node.querySelector('.params-container');
-    for (let paramName in params) {
-        const control = createParamControl(paramName, model);
-        paramContainer.appendChild(control);
-    }
-    const header = <HTMLDivElement>node.querySelector('.widget-header');
-    header.draggable = true;
-    header.ondragstart = (ev) => {
-        ev.dataTransfer.setData('id', node.id);
-        ev.dataTransfer.dropEffect = 'move';
-    };
-    return node;
-}
-
-function createParamControl (name: string, model: WidgetModel): HTMLElement {
+function createParamControl (name: string, model: WidgetModel): WidgetControlContainerPair {
     const param = model.opts.params[name];
     const node = document.createElement('div');
     node.className = 'param-control';
@@ -78,5 +86,13 @@ function createParamControl (name: string, model: WidgetModel): HTMLElement {
     }
     control.el.classList.add('param-input-container');
     node.appendChild(control.el);
-    return node;
-} 
+    return {
+        container: node,
+        control
+    };
+}
+
+interface WidgetControlContainerPair {
+    control: WidgetParamControl,
+    container: HTMLElement
+}

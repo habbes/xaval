38d9e8a8dc008c30edd6efe8a99de34464b11815
Clément Habinshuti <hello@habbes.xyz>
2018-06-28 03:35:51 +0300

    Add hidden green screen sample

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,6 +1,7 @@
 import CAMERA from './camera';
 import EDGE_DETECTION from './edge-detection';
 import QUICK_INTRO from './quick-intro';
+import SIMPLE_GREEN_SCREEN from './simple-green-screen';
 import VIDEO from './video';
 import WIDGETS from './widgets';
 
@@ -8,6 +9,7 @@ export {
     CAMERA,
     EDGE_DETECTION,
     QUICK_INTRO,
+    SIMPLE_GREEN_SCREEN,
     VIDEO,
     WIDGETS
 };
